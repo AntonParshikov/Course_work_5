58476fbe3f2cbb8f39f77326c62a02b0865f60c0
Anton Parshikov <tosha1703@gmail.com>
2023-06-22 16:03:33 +0300

    add fixes in dialog_function

--- dialog_function.py
+++ dialog_function.py
@@ -12,31 +12,28 @@ def user_interaction():
     clear_table()
     table_add_data()
 
-    user_input = input('Введите "1", чтобы получить список всех вакансий по запросу,\n'
-                       'Введите "2", чтобы получить среднюю зарплату по данным вакансиям,\n'
-                       'Введите "3", чтобы получить список всех вакансий,у которых зарплата выше средней,\n'
-                       'Нажмите "4", чтобы получить количество открытых вакансий компании\n')
+    # print('Введите "1", чтобы получить список всех вакансий по запросу,\n'
+    #       'Введите "2", чтобы получить среднюю зарплату по данным вакансиям,\n'
+    #       'Введите "3", чтобы получить список всех вакансий,у которых зарплата выше средней,\n'
+    #       'Введите "4", чтобы получить количество открытых вакансий компании\n')
 
     client = DBManager('Course_5_database')
 
-    if user_input == '1':
-        get_vacancies = client.get_all_vacancies()
-        print(get_vacancies)
+    enter_1 = input('Нажмите "Enter", чтобы получить список всех вакансий по запросу\n')
+    get_vacancies = client.get_all_vacancies()
+    print(get_vacancies)
 
-    user_input_1 = input()
-    if user_input_1 == '2':
-        avg_salary = client.get_avg_salary()
-        print(avg_salary)
+    enter_2 = input('Нажмите "Enter", чтобы получить среднюю зарплату по данным вакансиям\n')
+    avg_salary = client.get_avg_salary()
+    print(avg_salary)
 
-    user_input_2 = input()
-    if user_input_2 == '3':
-        higher_salary = client.get_vacancies_with_higher_salary()
-        print(higher_salary)
+    enter_3 = input('Нажмите "Enter", чтобы получить список всех вакансий,у которых зарплата выше средней\n')
+    higher_salary = client.get_vacancies_with_higher_salary()
+    print(higher_salary)
 
-    user_input_3 = input()
-    if user_input_3 == '4':
-        get_info = client.get_companies_and_vacancies_count()
-        print(get_info)
+    enter_4 = input('Нажмите "Enter", чтобы получить количество открытых вакансий компании\n')
+    get_info = client.get_companies_and_vacancies_count()
+    print(get_info)
 
     keyword = input('\nВведите ключевое слово для получения информации по конкретной вакансии: \n')
     vac_with_keyword = client.get_vacancies_with_keyword(keyword)
